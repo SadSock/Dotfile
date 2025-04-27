16498d64cb9009ba17a519e2d5f0727d841e01cf
sadsock <mantis-shrimp@outlook.com>
2025-04-27 15:43:26 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -213,7 +213,7 @@ wk.add(
 wk.add({
         {"<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>",  desc = "Next Hunk" },
         {"<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>",  desc = "Prev Hunk" },
-        {"<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>",  desc = "Blame" },
+        {"<leader>gl", "<cmd>Gitsigns toggle_current_line_blame <cr>",  desc = "Blame" },
         {"<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>",  desc = "Preview Hunk" },
         {"<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>",  desc = "Reset Hunk" },
         {"<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>",  desc = "Reset Buffer" },
