e06670295234217cb4b3e188d3b3b9e887f1355f
sadsock <mantis-shrimp@outlook.com>
2025-05-25 01:23:06 +0800

    update

--- nvim/lua/plugins/multicursors.lua
+++ nvim/lua/plugins/multicursors.lua
@@ -1,19 +1,42 @@
 return {
-{
+  {
     "smoka7/multicursors.nvim",
     event = "VeryLazy",
     dependencies = {
-        'nvimtools/hydra.nvim',
+      'nvimtools/hydra.nvim',
     },
-    opts = {},
+    opts = {
+
+      hint_config = {
+	float_opts = {
+	  border = 'rounded',
+	},
+	position = 'bottom-right',
+      },
+      generate_hints = {
+	normal = true,
+	insert = true,
+	extend = true,
+	config = {
+	  column_count = 1,
+	},
+      },
+
+    },
     cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
     keys = {
-            {
-                mode = { 'v', 'n' },
-                '<Leader>m',
-                '<cmd>MCstart<cr>',
-		desc = 'Multicursors',
-            },
-        },
-}
+      {
+	mode = { 'v' },
+	'<Leader>m',
+	'<cmd>MCvisual<cr>',
+	desc = 'Multicursors',
+      },
+      {
+	mode = { 'n' },
+	'<Leader>m',
+	'<cmd>MCunderCursor<cr>',
+	desc = 'Multicursors',
+      },
+    },
+  }
 }
